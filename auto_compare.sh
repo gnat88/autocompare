5d71c6c628ce7b6a448bd1f2fefd59b892f3439f
WeiTang <tangwei@nibirutech.com>
2014-10-17 10:19:54 +0800

    modify

--- auto_compare.sh
+++ auto_compare.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
+# $1-对比路径，$2-分支名称, $commit_id可选
 
 source compare_config
-if [ $# != 3 ]; then
+if [ $# -lt 2 ]; then
   echo "para error!"
   exit
 fi
@@ -9,16 +10,24 @@ fi
 cur=`pwd`
 cmppath=$1
 branch_a=$2
-commitid_a=$3
+
 
 cd $CMPWORK1
+# 新建分支
+cur_branch=`git branch | grep "*"`
+if [ "$cur_branch" == "* $branch_a" ]; then
+  git checkout master
+  git branch -D $branch_a
+  git checkout -b $branch_a origin/$branch_a
+fi
 
-git checkout master
-git branch -D $branch_a
-git checkout -b $branch_a origin/$branch_a
+# 拉取更新
 git pull origin $branch_a
-git checkout $commitid_a
+if [ $# -ge 3 ]; then
+  commitid_a=$3 
+  git checkout $commitid_a
+fi
 
-
+# 比较
 compare.sh $cur/$cmppath $CMPWORK1/$cmppath
 
